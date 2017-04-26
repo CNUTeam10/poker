157dd874ae9870cfc1469525a815942cc017fbc9
Hyunji Kim <hyunjivv@naver.com>
2017-04-27 01:21:18 +0900

    HJ add Player constructor

--- src/main/java/com/edu/cnu/poker/Player.java
+++ src/main/java/com/edu/cnu/poker/Player.java
@@ -4,5 +4,9 @@ package com.edu.cnu.poker;
  * Created by Hyunji Kim on 2017-04-27.
  */
 public class Player {
-    
+    Hand player_hand;
+
+    public Player(Deck player_deck){
+        player_hand = new Hand(player_deck, PokerType.FIVE);
+    }
 }
